5bf41b1aefbe4cd88c1adfab1684cdf02e53d695
niv-e <niveliyahu25@gmail.com>
2023-06-14 21:14:45 +0300

    Create a utilities project for common services

---
+++ Utilities/IdExists.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConnectFourGame.API.Utilities
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+           AllowMultiple = false)]
+    public class IdExists : ValidationAttribute
+    {
+        private readonly string _database;
+
+        public IdExists(string database)
+        { 
+            _database = database;
+        }
+        public override string FormatErrorMessage(string name)
+        {
+            return base.FormatErrorMessage("The given Id is already exists in the database");
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            //TODO: Impliment when a db will be added 
+            return ValidationResult.Success;
+
+
+
+        }
+    }
+}
